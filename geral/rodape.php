a749e89c9a28d98d5fcf44ff88d8ae60974de344
Michel Martins <129971241+Jebyyy@users.noreply.github.com>
2023-12-13 14:22:58 -0300

    Add files via upload

---
+++ geral/rodape.php
@@ -0,0 +1,18 @@
+<!-- Sobre -->
+<div id="Sobre" class="w3-modal w3-animate-opacity" >
+    <div class="w3-modal-content" style="max-width:500px">
+        <header class="w3-container w3-theme">
+            <span onclick="document.getElementById('Sobre').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+            <h2>Padaria</h2>
+        </header>
+        <div class="w3-container">
+            <p>ATP: <b>Fundamentos de Programação Web</b></p>
+            <p class="w3-small">Aluno: Michel Martins de Souza</p>
+        </div>
+        <footer class="w3-container w3-theme ">
+            <p>PUCPR 2023 </p>
+        </footer>
+    </div>
+</div>
+
+
